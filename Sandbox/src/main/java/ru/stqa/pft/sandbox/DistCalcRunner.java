edc0f655761595c395d27e1ec39724c003692319
ViktorKovalenko <vsutogan@gmail.com>
2019-03-06 10:44:21 +0200

    Созданы классы для вычисления расстояния между точками

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/DistCalcRunner.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class DistCalcRunner {
+
+    public static void main(String[] args) {
+
+
+      Point p1 = new Point(3, 2);
+      Point p2 = new Point(3, 5);
+        System.out.println( "Расстояние между двумя точками равно " + Point.distance(p1, p2));
+    }
+}
