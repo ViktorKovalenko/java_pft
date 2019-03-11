89746a902a68b7f211bbd68727943b88d2efde7d
ViktorKovalenko <vsutogan@gmail.com>
2019-03-11 11:55:17 +0200

    Исправления в тесте.  Метод осоциирован с объектом.

--- Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,6 +8,6 @@ public class PointTests {
     public void testDistance() {
         Point p1 = new Point(3, 2);
         Point p2 = new Point(3, 5);
-        Assert.assertEquals(Point.distance(p1, p2), 3);
+        Assert.assertEquals(p1.distance(p2), 3);
     }
 }
