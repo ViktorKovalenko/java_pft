b15fc604975abbab85cf39f88d7f4b38df493756
ViktorKovalenko <vsutogan@gmail.com>
2019-03-15 21:38:24 +0200

    Конструции if ,else

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,74 @@
+package ru.stqa.pft.sandbox;
+
+
+public class Equation {
+
+    private int n;
+
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public Equation(double a, double b, double c) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+
+        double d = b * b - 4 * a * c;
+        if (a != 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+
+            }
+        } else if (b != 0) {
+            n = 1;
+        } else if (c != 0) {
+            n = 0;
+        } else {
+
+            n = -1;
+        }
+
+
+    }
+
+
+
+/*
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else {
+                    n = 0;
+                }
+            } else {
+                n = 1;
+            }
+
+
+        }else {
+            if (d > 0) {
+            n = 2;
+        } else if (d == 0) {
+            n = 1;
+        }else{
+        n = 0;
+
+    }
+    }
+
+}
+*/
+
+
+    public int rootNumber() {
+        return n;
+    }
+}
