dc62c73074fde73bfa8ca8dcb43436b9f76b1b98
ViktorKovalenko <vsutogan@gmail.com>
2019-04-10 22:54:12 +0300

    Добавлено протоколирование(лог файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static org.openqa.selenium.remote.BrowserType.CHROME;
+import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
 public class TestBase {
 
-
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final AplicationManager app
             = new AplicationManager(System.getProperty("browser", CHROME));
 
@@ -27,4 +35,13 @@ public class TestBase {
     public AplicationManager getApp() {
         return app;
     }
+
+@BeforeMethod
+public void logTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+}
+@AfterMethod(alwaysRun = true)
+public void logTestStop(Method m){
+    logger.info("Stop test " + m.getName());
+}
 }
